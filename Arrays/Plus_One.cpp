91d572e6c780e90d47fb95d607be7d19d8b17d88
Hamid-Fastian-AI <malikhamidraza12232@gmail.com>
2026-09-10 19:53:40 +0500

    Refactor PlusOne function for carry handling
    Refactor PlusOne function to handle carry propagation correctly and improve readability.

--- Arrays/Plus_One.cpp
+++ Arrays/Plus_One.cpp
@@ -4,57 +4,62 @@ using namespace std;
 
 vector<int> PlusOne(vector<int>& digits)
 {
-	int r, q;
-	for (int i = 0; i < digits.size(); i++)
-	{
-		if (i == digits.size() - 1)
-		{
-			if (digits[i] + 1 >= 10)
-			{
-				if (digits.size() == 1 )
-				{
-					r = (digits[i] + 1) % 10;
-					q = (digits[i] + 1) / 10;
-					digits[i] = q;
-					digits.push_back(r);
-					break;
-				}
-				else
-				{
-					while (digits[i] >= 9)
-					{
-						r = (digits[i] + 1) % 10;
-						q = (digits[i] + 1) / 10;
-						digits[i] = r;
-					    digits[i - 1] += q;
-						i--;
+    int r, q;
 
-					};
-					break;
-				}
-				
+    for (int i = 0; i < digits.size(); i++)
+    {
+        if (i == digits.size() - 1)
+        {
+            if (digits[i] + 1 >= 10)
+            {
+                if (digits.size() == 1)
+                {
+                    r = (digits[i] + 1) % 10;
+                    q = (digits[i] + 1) / 10;
 
-			}
-			else 
-			{
-				digits[i] += 1;
-			}
-				
-		}
-	}
-	return digits;
-}
+                    digits[i] = q;
+                    digits.push_back(r);
 
-int main()
-{
-	vector<int> nums = {1,9,9};
-	vector<int> vec = PlusOne(nums);
-	cout << "Resulted integer: ";
-	for (int i = 0; i < vec.size(); i++)
-	{
-		cout << vec[i];
-	}
-	cout << endl;
-	system("pause");
-	return 0;
+                    break;
+                }
+                else
+                {
+                    r = (digits[i] + 1) % 10;
+                    q = (digits[i] + 1) / 10;
+
+                    digits[i] = r;
+
+                    int j = i - 1;
+
+                    while (j >= 0 && q > 0)
+                    {
+                        if (digits[j] + q >= 10)
+                        {
+                            digits[j] = (digits[j] + q) % 10;
+                            q = 1;
+                            j--;
+                        }
+                        else
+                        {
+                            digits[j] += q;
+                            q = 0;
+                        }
+                    }
+
+                    if (q > 0)
+                    {
+                        digits.insert(digits.begin(), q);
+                    }
+
+                    break;
+                }
+            }
+            else
+            {
+                digits[i] += 1;
+            }
+        }
+    }
+
+    return digits;
 }
